ec3cde1ee9a3af2dbeab312c1a3d2796d66f80de
André Victor <andre.victorueg@gmail.com>
2024-02-02 16:19:33 -0300

    Ajustes no tamanho das figurinhas

--- assets/js/figurinhas.js
+++ assets/js/figurinhas.js
@@ -1,11 +1,29 @@
 $(document).ready(() => {
     $('.grid').masonry({
         itemSelector: '.grid-item',
-        columnWidth: 200,
+        columnWidth: 0,
         percentPosition: true
     })
 
     $('.image').on("load", () => {
+        loadHeigth()
         $('.grid').masonry()
     })
 })
+
+function loadHeigth(){
+    var c = [];
+       
+    $("#div_imagens > div").each(function(){
+        for(var i=0; i < $(this).length; i++){
+            var height = $(this).eq(i).outerHeight();
+            c.push(height)
+       }
+    
+     });
+    
+     var max = Math.max.apply(Math,c)
+     if(max > 600 )
+        max = 500
+    $("#div_imagens > div").css("min-height", max + "px");
+}
